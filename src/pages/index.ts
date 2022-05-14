f08fdaa99be110d3e52bf7ab215b03f705ef1a8e
tipup-manager <tipup-manager@gmail.com>
2022-05-14 22:47:25 +0900

    update package version

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,5 @@
 export { default as Simple404 } from "./simple404/Simple404";
 export { default as Warning404 } from "./warning404/Warning404";
 export { default as Section404 } from "./section404/Section404";
+export { default as Cat404 } from "./cat404/Cat404";
+export { default as Blocked404 } from "./blocked404/Blocked404";
